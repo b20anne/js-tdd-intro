6935f4dcbdd29b2755436cdf7bd1b534605a27a9
b20anne <b20anne@yahoo.fr>
2021-02-01 13:06:49 +0100

    writing test

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+//   is a function that accepts one argument
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+assert.strictEqual(capitalizeFirstLetters.length, 1);
+
+// transforms one word correctly
+assert.strictEqual(capitalizeFirstLetters('capital'), 'Capital');
+
+// works for a 1-character string
+assert.strictEqual(capitalizeFirstLetters('a'), 'A');
+
+// works with multiple words
+assert.strictEqual(capitalizeFirstLetters("i am learning TDD"), "I Am Learning TDD");
+
+// works for an empty string
+assert.strictEqual(capitalizeFirstLetters(''), '');
